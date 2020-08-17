3c317372388a7271ded1344d87dcf803377d631f
juan chan <juan_chan@MacBook-Pro-de-juan.local>
2020-08-17 02:18:14 -0500

    primera carga

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 import 'package:disenos/src/pages/basico_page.dart';
 import 'package:disenos/src/pages/scroll_page.dart';
 import 'package:disenos/src/pages/botones_page.dart';
 
-
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-
-    SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light.copyWith(
-      statusBarColor: Colors.transparent
-    ));
-
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.transparent));
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'basico',
+      initialRoute: 'scroll',
       routes: {
-        'basico'  : (BuildContext context ) => BasicoPage(),
-        'scroll'  : (BuildContext context ) => ScrollPage(),
-        'botones' : (BuildContext context ) => BotonesPage(),
+        'basico': (BuildContext context) => BasicoPage(),
+        'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage(),
       },
     );
   }
-
 }
